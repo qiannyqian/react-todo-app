237bc4df99d9875fd686b918f60b73c639ab220d
qiannyqian <liang.huiqian@gmail.com>
2020-05-11 16:06:48 +0800

    Styled app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    newItem: '',
+    list: [],
+  };
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list: list,
+      newItem: '',
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+
+    const updatedList = list.filter((item) => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3 className="title-text">My To-Do List</h3>
+
+        <div>
+          Put down a to-do here...
+          <br />
+          <input
+            type="text"
+            placeholder="Type Something Here..."
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput('newItem', e.target.value)}
+          />
+          <button className="add-button" onClick={() => this.addItem()}>
+            Add
+          </button>
+        </div>
+
+        <ul className={this.state.list.length > 0 ? 'items-list' : null}>
+          {this.state.list.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <button
+                  className="delete-button"
+                  onClick={() => this.deleteItem(item.id)}
+                >
+                  X
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
